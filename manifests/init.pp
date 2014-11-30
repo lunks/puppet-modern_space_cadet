87b0341ea9f4e1230e06c0e04e0d98914cc55b2a
Pedro Nascimento <pnascimento@gmail.com>
2014-11-30 02:12:10 -0200

    Too stupid to figure out some of these options. :P
    changing caps to esc, too

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,15 +4,12 @@
 #
 #   include modern_space_cadet
 class modern_space_cadet(
-  $capslock_to_control_plus_escape = true,
   $force_correct_shifts            = true,
   $shifts_to_parens                = true,
-  $left_control_to_hyper           = true,
   $fix_opt_arrows                  = true,
   $math_layer                      = true,
   $greek_layer                     = true
 ) {
-  $left_control_to_hyper_val = $left_control_to_hyper ? { true => 1, false => 0 }
   $force_correct_shifts_val  = $force_correct_shifts  ? { true => 1, false => 0 }
   $shifts_to_parens_val      = $shifts_to_parens      ? { true => 1, false => 0 }
   $fix_opt_arrows_val        = $fix_opt_arrows        ? { true => 1, false => 0 }
@@ -24,19 +21,11 @@ class modern_space_cadet(
 
   include karabiner
   karabiner::profile{ 'Default': }
+(
+  seil::map { 'capslock': value => 53}
+  
 
-  if ($capslock_to_control_plus_escape) {
-    karabiner::remap{ 'controlL2controlL_escape': }
-    karabiner::set{ 'parameter.keyoverlaidmodifier_timeout': value => 300 }
-
-    seil::map { 'capslock': value => 59}
-  }
-
-  if ($left_control_to_hyper) {
-    seil::map { 'control_l': value => 80}
-  }
-
-  karabiner::set{ 'space_cadet.left_control_to_hyper': value => $left_control_to_hyper_val }
+  karabiner::set{ 'space_cadet.left_control_to_hyper': value => 0 }
   karabiner::set{ 'space_cadet.force_correct_shifts':  value => $force_correct_shifts_val }
   karabiner::set{ 'space_cadet.shifts_to_parens':      value => $shifts_to_parens_val }
   karabiner::set{ 'space_cadet.fix_opt_arrows':        value => $fix_opt_arrows_val }
